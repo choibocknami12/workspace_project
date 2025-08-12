b6631f49f7c6cfa0d0a0514934e6d0cdf8aa3367
choibocknami12 <h_hcho1215@naver.com>
2025-08-12 22:24:44 +0900

    java연습1일차

--- Java_miniboard/test/Test.java
+++ Java_miniboard/test/Test.java
@@ -1,7 +1,18 @@
 package Java_miniboard.test;
 
 public class Test {
-    public static void main(String[] args) {
-        System.err.println("Hello!");
+    private String message;
+    private Integer message1;
+
+    public void hello(String message) {
+        System.out.println("Hello!");
+    }
+
+    public void bye(String arg) {
+        System.out.println("good bye!" + arg);
+    }
+
+    public void num(Integer message1) {
+        System.out.println("good bye!" + message1);
     }
 }
---
+++ Java_miniboard/test/Test2.java
@@ -0,0 +1,11 @@
+package Java_miniboard.test;
+
+public class Test2 {
+    public static void main(String[] arg) {
+        Test t = new Test();
+        t.bye("복남");
+        t.num(1);
+
+    }
+
+}
