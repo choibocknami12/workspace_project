0373c47dfef001acd7c3a7434c19b67ea6aa32a2
choibocknami12 <h_hcho1215@naver.com>
2025-11-15 21:59:07 +0900

    정규식 내용수정

--- Python/ex/ex4.py
+++ Python/ex/ex4.py
@@ -7,17 +7,19 @@ def clean_text(text):
     text = "내 마음 대로 <div>test</div> 하기"
     
     if isinstance(text, str):
-        text = re.sub(r'<[^>]*>','', text)
-        text = re.sub(r'[^a-zA-Z0-9가-힣\s]','',text)
-        # text = re.sub(r'<[^>]*>','', text)
-        text = text.lower()
+        text = re.sub(r'<[^>]*>','', text) #3
+        text = re.sub(r'[^a-zA-Z0-9가-힣\s]','',text) #1
+        # text = re.sub(r'<[^>]*>','', text) #2
+        # ---------------------------------------------------------
+        # 교재에선 1,2 순서로 해두었지만, 그렇게되면 html코드가 지워지지않음.
+        # 3,1 순서로 진행해야 깨끗한 텍스트정제가 가능함.
+        # 함수사용시 리턴값 잊지말자.
 
         return text
     else:
         return ''
 
-# def clean_df(df):
-#     if not df.empty:
-#         df['정제된 제목'] = df['제목'].apply(clean_text)
-
 print(clean_text(""))
+# 호출에러 오답노트
+# print(clean_text()) : TypeError. 함수에 1개의 인자를 가지고 있으니까 꼭 추가해줘야함
+# print(clean_text(text)) : NameError. 함수안에서 print한게 아니라서 text에 대한 값이 없음.
